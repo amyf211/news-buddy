efc1022023f8ec158cf6b6c3346886b7b5e5328d
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-03-11 16:33:41 +0000

    update routes

--- news-buddy/src/App.jsx
+++ news-buddy/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/> } />
         <Route path='/articles' element={<ArticlesList/> } />
+        <Route path='/articles' element={<SingleArticle/> } />
       </Routes>
     </>
   )
